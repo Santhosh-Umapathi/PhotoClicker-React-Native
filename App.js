236afd3466ad872e415c5dd750ec674a3abc24b0
Santhosh Umapathi <Santhosh.professional1@gmail.com>
2020-03-31 19:01:44 +0200

    Navigator installed

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+//import FollowScreen from "./src/screens/FollowScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const MainNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+   // Follow: FollowScreen
   },
-});
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Home",
+      headerTintColor: "white", //Header button colors
+      headerStyle: {
+        backgroundColor: "lightblue" // header bg color
+      },
+      headerTitleStyle: {
+        color: "black" // header font color
+      }
+    }
+  }
+);
+
+export default createAppContainer(MainNavigator);
